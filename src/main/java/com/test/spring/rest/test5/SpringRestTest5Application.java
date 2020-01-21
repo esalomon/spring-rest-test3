020d11d249be3f85fc4fd16b240521e0060f91d8
Efrain Salomon <efrain@prominic.net>
2020-01-21 15:00:57 -0600

    The original application was removed in order to avoid conflicts when the second application was added. The second application receives two integer arrays and the service returns one ordered list.

--- src/main/java/com/test/spring/rest/test5/SpringRestTest5Application.java
+++ src/main/java/com/test/spring/rest/test5/SpringRestTest5Application.java
@@ -1,13 +1,13 @@
-package com.demo.spring.rest.test3;
+package com.test.spring.rest.test5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRestTest3Application {
+public class SpringRestTest5Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringRestTest3Application.class, args);
+        SpringApplication.run(SpringRestTest5Application.class, args);
     }
 
 }
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into user (id, first_name, last_name) values ('101', 'John', 'Smith');
-insert into user (id, first_name, last_name) values ('102', 'Joel', 'Jones');
-insert into user (id, first_name, last_name) values ('103', 'Jone', 'Brow');
